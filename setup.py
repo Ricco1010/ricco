010f486fca8b06bd5a769838053616d97a7d1892
Ricco <wyk_0610@163.com>
2020-07-08 09:24:43 +0800

    增加必要的包

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ from setuptools import setup
 pwd = os.path.dirname(__file__)
 
 with open(os.path.join(pwd, 'src', 'ricco', '__init__.py')) as f:
-  VERSION = (
-      re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S)
-      .match(f.read())
-      .group(1)
-  )
+    VERSION = (
+        re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S)
+            .match(f.read())
+            .group(1)
+    )
 
 with open(os.path.join(pwd, 'README.md'), encoding='utf-8') as f:
-  README = f.read()
+    README = f.read()
 
 setup(name='ricco',
       version=VERSION,
@@ -27,12 +27,15 @@ setup(name='ricco',
       author_email="wyk_0610@163.com",
       packages=find_packages('src'),
       package_dir={
-        '': 'src',
+          '': 'src',
       },
       platforms='any',
       install_requires=[
-        'numpy>=0.17',
-        'pandas>=0.24'
+          'numpy>=0.17',
+          'pandas>=0.24',
+          'geojson>=2.5.0',
+          'pypinyin>=0.38.0'
+
       ],
       classifiers=[
           'Development Status :: 3 - Alpha',
