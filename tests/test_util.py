d1c82055c29c68fdf47504427abb783b83ab9de6
Ricco <wyk_0610@163.com>
2020-07-16 14:24:25 +0800

    增加测试

--- tests/test_util.py
+++ tests/test_util.py
@@ -12,6 +12,9 @@ from ricco.util import to_float
 
 def test_add():
     assert add(1, 1) == 2
+    assert add('1', 1) == 2.0
+    assert add(1, '1') == 2.0
+    assert add('1', '1') == 2.0
 
 
 def test_pinyin():
