979f0780fc54c400e43c614d75dbb47d5d494ebe
wangyukang <wangyukang@metrodata.cn>
2024-01-31 18:44:19 +0800

    Bump version: 1.3.5 → 1.4.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -10,7 +10,7 @@ project = 'ricco'
 copyright = '2024, wangyukang'
 author = 'wangyukang'
 
-release = '1.3.5'
+release = '1.4.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
